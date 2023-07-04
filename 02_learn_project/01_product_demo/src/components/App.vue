fb542712b9b902f769d58bd2090d5ea0c3cc7fbf
hemin <1530781589@qq.com>
2023-07-04 09:07:45 +0800

    全局组件和局部组件

---
+++ 02_learn_project/01_product_demo/src/components/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>{{ title }}</h2>
+  <h2>当前计数: {{ counter }}</h2>
+  <button @click="decrement">+1</button>
+  <button @click="increment">-1</button>
+
+  <!-- 组件 -->
+  <product-item></product-item>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue'
+
+export default {
+  components: {
+    ProductItem
+  },
+  data() {
+    return {
+      title: "这是一个标题",
+      counter: 0
+    }
+  },
+  methods: {
+    decrement() {
+      this.counter++
+    },
+    increment() {
+      this.counter--
+    }
+  }
+}
+</script>
+
+<style>
+h2 {
+  color: red;
+}
+</style>
---
+++ 02_learn_project/01_product_demo/src/components/ProductItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>{{ title }}</h2>
+  <p>商品描述, 9.9秒杀</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "这是一个组件"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
